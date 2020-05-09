0e17ce381597389e6734a9243f105483fbea63cb
Tyler McBride <tymcbrid@gmail.com>
2020-05-09 11:08:02 -0700

    routes

--- server.js
+++ server.js
@@ -34,3 +34,15 @@ var newVar = [
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
+
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+  app.get("/api/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
